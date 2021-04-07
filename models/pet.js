98bb0d07eb25a17a1f7264692e4bc8475ec10021
shaannessy25 <shaan.hurley25@gmail.com>
2021-04-06 20:14:59 -0700

    Implemented pagination on the home page

--- models/pet.js
+++ models/pet.js
@@ -1,19 +1,28 @@
 "use strict";
 
-const mongoose = require('mongoose'),
-        Schema = mongoose.Schema;
+const mongoose = require("mongoose"),
+  Schema = mongoose.Schema;
 
-const PetSchema = new Schema({
-    name            : { type: String, required: true }
-  , species         : { type: String }
-  , birthday        : { type: Date }
-  , picUrl          : { type: String }
-  , picUrlSq        : { type: String }
-  , favoriteFood    : { type: String }
-  , description     : { type: String }
-},
-{
-  timestamps: true
-});
+const mongoosePaginate = require("mongoose-paginate");
 
-module.exports = mongoose.model('Pet', PetSchema);
+mongoosePaginate.paginate.options = {
+  limit: 3, // how many records on each page
+};
+
+const PetSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    species: { type: String },
+    birthday: { type: Date },
+    picUrl: { type: String },
+    picUrlSq: { type: String },
+    favoriteFood: { type: String },
+    description: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+PetSchema.plugin(mongoosePaginate);
+module.exports = mongoose.model("Pet", PetSchema);
